799c4eb5c61afba11901f25b71ca07f9f7652589
Wang Yu <wangyu_onepach@qq.com>
2019-02-27 18:15:20 +0800

    verify code delete lines

--- headless/headlessTest.py
+++ headless/headlessTest.py
@@ -1,7 +1,20 @@
 import time
 
 from headless.MyHeadless import MyHeadless
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as expected
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from PIL import Image
+
+
+def has_tag(driver, selector):
+    try:
+        driver.find_element_by_css_selector(selector)
+    except NoSuchElementException as e:
+        return False
+    return True
+
 
 if __name__ == '__main__':
     t_url = "https://login.taobao.com/member/login.jhtml"
@@ -33,10 +46,12 @@ if __name__ == '__main__':
         username_input = driver.find_element_by_css_selector('#pl_login_logged > div > div:nth-child(2) > div > input')
         # 密码
         password_input = driver.find_element_by_css_selector('#pl_login_logged > div > div:nth-child(3) > div > input')
-        # checkbox
-        check_box = driver.find_element_by_css_selector('#login_form_savestate')
+        # check box
+        check_box = driver.find_element_by_css_selector("#login_form_savestate")
+
         # 登录按钮
         login_btn = driver.find_element_by_css_selector('#pl_login_logged > div > div:nth-child(7) > div:nth-child(1) > a')
+
         username_input.clear()
         username_input.send_keys('13820552740')
         password_input.send_keys('wang60589537')
@@ -46,29 +61,28 @@ if __name__ == '__main__':
         time.sleep(5)
 
         verify_img = None
-        no_img = True
-        while no_img:
-            try:
-                verify_img = driver.find_element_by_css_selector('#pl_login_logged > div > div:nth-child(4) > div > a.code > img')
-                no_img = False
-            except driver.NoSuchElementException as e:
-                print("验证码没有出现在页面上，重新登录以显示")
-                login_btn.click()
-                time.sleep(3)
-        img_location = verify_img.location
-        img_size = verify_img.size
-        left = img_location['x']
-        top = img_location['y']
-        right = left + img_size['width']
-        bottom = top + img_size['height']
+        is_login_right = True
 
-        screen_shot = 'full_screen.png'
-        driver.save_screenshot(screen_shot)
-        # 保存当前验证码
-        temp_verify_code = Image.open(screen_shot).crop((left, top, right, bottom))
-        temp_verify_code.save('verify_code.png')
+        if has_tag(driver, 'body > div.layer_form_tips > div > div.content > p'):
+            print(driver.find_element_by_css_selector('body > div.layer_form_tips > div > div.content > p').text)
 
+        expect_element = expected.presence_of_element_located((By.CSS_SELECTOR, "#J_SiteNavLogin > div.site-nav-menu-hd > div.site-nav-user > a"))
+        ele = WebDriverWait(driver, 10, ignored_exceptions=NoSuchElementException).until(expect_element, "time out")
+        print(ele)
 
-
+        # img_location = verify_img.location
+        # img_size = verify_img.size
+        # left = img_location['x']
+        # top = img_location['y']
+        # right = left + img_size['width']
+        # bottom = top + img_size['height']
+        #
+        # screen_shot = 'full_screen.png'
+        # driver.save_screenshot(screen_shot)
+        # # 保存当前验证码
+        # temp_verify_code = Image.open(screen_shot).crop((left, top, right, bottom))
+        # temp_verify_code.save('verify_code.png')
     # driver.close()
+
+
 
