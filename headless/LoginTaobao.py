774281f67be90e22a00aed1494aa715f137bbf04
Wang Yu <wangyu_onepach@qq.com>
2019-03-05 18:16:46 +0800

    add LoginTaobao.py to access taobao goods

--- headless/LoginTaobao.py
+++ headless/LoginTaobao.py
@@ -12,18 +12,11 @@ def get_element(driver, by, tag_name, time_out=10):
 
 if __name__ == '__main__':
     t_url = "https://detail.tmall.com/item.htm?spm=a230r.1.14.48.2822476ehRVzj6&id=570269376065&ns=1&abbucket=19"
-    t_login_frame = 'sufei-dialog-content'
 
     headless = MyHeadless(url=t_url)
     driver = headless.getDriver(False)
     driver.maximize_window()
     driver.get(headless.url)
-    driver.switch_to_frame(t_login_frame)
-    user_name_input = driver.find_element_by_css_selector('#TPL_username_1')
-    password_input = driver.find_element_by_css_selector('#TPL_password_1')
-    user_name_input.send_keys('13820552740')
-    password_input.send_keys('wang254841204')
-
     webdriver_false = """ Object.defineProperties(navigator,{
                  webdriver:{
                    get: () => false
@@ -31,6 +24,21 @@ if __name__ == '__main__':
                })"""
     # 让webdriver = false
     driver.execute_script(script=webdriver_false)
+    login_link = driver.find_element_by_css_selector('#login-info > a.sn-login')
+    if login_link.text == '请登录':
+        login_link.click()
+    user_label = get_element(driver, By.CSS_SELECTOR,
+                          '#login-info > span:nth-child(1) > a.j_Username.j_UserNick.sn-user-nick', 60)
+    print(user_label.text)
 
+    goods_name = driver.find_element_by_css_selector('#J_DetailMeta > div.tm-clear > div.tb-property > div > div.tb-detail-hd > h1')
+    print("商品名称:", goods_name)
     comment_num = get_element(driver, By.CSS_SELECTOR, '#J_TabBar > li:nth-child(2) > a > em')
-    print(comment_num.text)
+    print("评论数:",comment_num.text)
+    comment_content = driver.find_element_by_css_selector('#J_TabBar > li.tm-selected > a')
+    # 点击评论
+    comment_content.click()
+    tbody = driver.find_elements_by_xpath('//*[@id="J_Reviews"]/div/div[6]/table/tbody/tr')
+    for tr in tbody:
+        comment_text = tr.find_elements_by_xpath('td/div/div')
+        print(comment_text)
